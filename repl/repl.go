f0b2dbf4109cd7fcdfb5d4972b1b1d34672312ad
fd1az <fdiaz.dev@gmial.com>
2024-03-11 11:29:52 -0300

    feat: Added AST implementation and parser for Monkey language

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/fd1az/monkey/lexer"
-	"github.com/fd1az/monkey/token"
+	"github.com/fd1az/monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -16,7 +16,6 @@ func Start(in io.Reader, out io.Writer) {
 
 	for {
 		fmt.Fprintf(out, PROMPT)
-
 		scanned := scanner.Scan()
 		if !scanned {
 			return
@@ -24,9 +23,37 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
