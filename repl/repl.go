b916cf90d58d480a69955fb729a336a6bb349b83
fd1az <fdiaz.dev@gmial.com>
2024-07-09 17:19:44 -0300

    eval

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/fd1az/monkey/evaluator"
 	"github.com/fd1az/monkey/lexer"
+	"github.com/fd1az/monkey/object"
 	"github.com/fd1az/monkey/parser"
 )
 
@@ -13,10 +15,12 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
-		fmt.Fprintf(out, PROMPT)
+		fmt.Fprint(out, PROMPT, "")
 		scanned := scanner.Scan()
+
 		if !scanned {
 			return
 		}
@@ -31,8 +35,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program, env)
+
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
